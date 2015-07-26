053be1eb19f063c8fa1662c69958cb989f35d5b0
Francisco Saldana <francisco@ginger.io>
2015-07-26 01:28:52 -0700

    initial game

---
+++ observable.js
@@ -0,0 +1,237 @@
+;(function () {
+
+// bind a to b -- One Way Binding
+function bind1(a, b) {
+  a(b()); b(a)
+}
+//bind a to b and b to a -- Two Way Binding
+function bind2(a, b) {
+  b(a()); a(b); b(a);
+}
+
+//---util-funtions------
+
+//check if this call is a get.
+function isGet(val) {
+  return undefined === val
+}
+
+//check if this call is a set, else, it's a listen
+function isSet(val) {
+  return 'function' !== typeof val
+}
+
+function isFunction (fun) {
+  return 'function' === typeof fun
+}
+
+function assertObservable (observable) {
+  if(!isFunction(observable))
+    throw new Error('transform expects an observable')
+  return observable
+}
+
+//trigger all listeners
+function all(ary, val) {
+  for(var k in ary)
+    ary[k](val)
+}
+
+//remove a listener
+function remove(ary, item) {
+  delete ary[ary.indexOf(item)]
+}
+
+//register a listener
+function on(emitter, event, listener) {
+  (emitter.on || emitter.addEventListener)
+    .call(emitter, event, listener, false)
+}
+
+function off(emitter, event, listener) {
+  (emitter.removeListener || emitter.removeEventListener || emitter.off)
+    .call(emitter, event, listener, false)
+}
+
+//An observable that stores a value.
+
+function value (initialValue) {
+  var _val = initialValue, listeners = []
+  observable.set = function (val) {
+    all(listeners, _val = val)
+  }
+  return observable
+
+  function observable(val) {
+    return (
+      isGet(val) ? _val
+    : isSet(val) ? all(listeners, _val = val)
+    : (listeners.push(val), val(_val), function () {
+        remove(listeners, val)
+      })
+  )}}
+  //^ if written in this style, always ends )}}
+
+/*
+##property
+observe a property of an object, works with scuttlebutt.
+could change this to work with backbone Model - but it would become ugly.
+*/
+
+function property (model, key) {
+  return function (val) {
+    return (
+      isGet(val) ? model.get(key) :
+      isSet(val) ? model.set(key, val) :
+      (on(model, 'change:'+key, val), val(model.get(key)), function () {
+        off(model, 'change:'+key, val)
+      })
+    )}}
+
+/*
+note the use of the elvis operator `?:` in chained else-if formation,
+and also the comma operator `,` which evaluates each part and then
+returns the last value.
+
+only 8 lines! that isn't much for what this baby can do!
+*/
+
+function transform (observable, down, up) {
+  assertObservable(observable)
+  return function (val) {
+    return (
+      isGet(val) ? down(observable())
+    : isSet(val) ? observable((up || down)(val))
+    : observable(function (_val) { val(down(_val)) })
+    )}}
+
+function not(observable) {
+  return transform(observable, function (v) { return !v })
+}
+
+function listen (element, event, attr, listener) {
+  function onEvent () {
+    listener(isFunction(attr) ? attr() : element[attr])
+  }
+  on(element, event, onEvent)
+  onEvent()
+  return function () {
+    off(element, event, onEvent)
+  }
+}
+
+//observe html element - aliased as `input`
+function attribute(element, attr, event) {
+  attr = attr || 'value'; event = event || 'input'
+  return function (val) {
+    return (
+      isGet(val) ? element[attr]
+    : isSet(val) ? element[attr] = val
+    : listen(element, event, attr, val)
+    )}
+}
+
+// observe a select element
+function select(element) {
+  function _attr () {
+      return element[element.selectedIndex].value;
+  }
+  function _set(val) {
+    for(var i=0; i < element.options.length; i++) {
+      if(element.options[i].value == val) element.selectedIndex = i;
+    }
+  }
+  return function (val) {
+    return (
+      isGet(val) ? element.options[element.selectedIndex].value
+    : isSet(val) ? _set(val)
+    : listen(element, 'change', _attr, val)
+    )}
+}
+
+//toggle based on an event, like mouseover, mouseout
+function toggle (el, up, down) {
+  var i = false
+  return function (val) {
+    function onUp() {
+      i || val(i = true)
+    }
+    function onDown () {
+      i && val(i = false)
+    }
+    return (
+      isGet(val) ? i
+    : isSet(val) ? undefined //read only
+    : (on(el, up, onUp), on(el, down || up, onDown), val(i), function () {
+      off(el, up, onUp); off(el, down || up, onDown)
+    })
+  )}}
+
+function error (message) {
+  throw new Error(message)
+}
+
+function compute (observables, compute) {
+  var cur = observables.map(function (e) {
+    return e()
+  }), init = true
+
+  var v = value()
+
+  observables.forEach(function (f, i) {
+    f(function (val) {
+      cur[i] = val
+      if(init) return
+      v(compute.apply(null, cur))
+    })
+  })
+  v(compute.apply(null, cur))
+  init = false
+  v(function () {
+    compute.apply(null, cur)
+  })
+
+  return v
+}
+
+function boolean (observable, truthy, falsey) {
+  return (
+    transform(observable, function (val) {
+      return val ? truthy : falsey
+    }, function (val) {
+      return val == truthy ? true : false
+    })
+  )
+}
+
+function signal () {
+  var _val, listeners = []
+  return function (val) {
+    return (
+      isGet(val) ? _val
+        : isSet(val) ? (!(_val===val) ? all(listeners, _val = val):"")
+        : (listeners.push(val), val(_val), function () {
+           remove(listeners, val)
+        })
+    )}}
+
+var exports = value
+exports.bind1     = bind1
+exports.bind2     = bind2
+exports.value     = value
+exports.not       = not
+exports.property  = property
+exports.input     =
+exports.attribute = attribute
+exports.select    = select
+exports.compute   = compute
+exports.transform = transform
+exports.boolean   = boolean
+exports.toggle    = toggle
+exports.hover     = function (e) { return toggle(e, 'mouseover', 'mouseout')}
+exports.focus     = function (e) { return toggle(e, 'focus', 'blur')}
+exports.signal    = signal
+
+if('object' === typeof module) module.exports = exports
+else                           this.observable = exports
+})()
