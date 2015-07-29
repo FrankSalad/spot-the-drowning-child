fac512ffbc26c61bae61670d7b73784143aaf3e9
Francisco Saldana <francisco@ginger.io>
2015-07-29 09:02:22 -0700

    Move videos into separate file. Creator tool anchors look up game state in list of games.

--- game.js
+++ game.js
@@ -59,45 +59,6 @@
   var successMsgs = ['Good job.', 'Nice work.', 'Good eye.', 'Well done.', 'Nicely done.'];
   var pauseMsgs = ['Looks fine there.', 'Seems ok.', 'All clear there.', 'Just splashing around.'];
 
-  var videos = [{
-    videoId: '4sFuULOY5ik',
-    drowningStartSecs: 12.2,
-    whistleSecs: 14.5,
-    swimmerSavedSecs: 25,
-    findBoxStyle: "left: 455px; top: -365px; position: relative; width: 50px; height: 34px;"
-  }, {
-    videoId: 'JYQ9AwsTkAw',
-    drowningStartSecs: 18.4,
-    whistleSecs: 21,
-    swimmerSavedSecs: 28.5,
-    findBoxStyle: "left: 383px; top: -326px; position: relative; width: 80px; height: 65px;"
-  }, {
-    videoId: 'PuAfTA2wf7o',
-    drowningStartSecs: 37.3,
-    whistleSecs: 45.1,
-    swimmerSavedSecs: 50.5,
-    findBoxStyle: "left: 329px; top: -385px; position: relative; width: 103px; height: 37px;"
-  }, {
-    videoId: 'T5mDQeDkca0',
-    drowningStartSecs: 54.0,
-    whistleSecs: 56.5,
-    swimmerSavedSecs: 62.8,
-    findBoxStyle: "left: 606px; top: -265px; position: relative; width: 102px; height: 102px;",
-  }, {
-    "videoId": "L0KTqPloUiU",
-    "drowningStartSecs": 31.506069,
-    "whistleSecs": 37.946069,
-    "swimmerSavedSecs": 45.126069,
-    "findBoxStyle": "left: 601px; top: -302px; position: relative; width: 203px; height: 136px;"
-    },
-    {
-    "videoId": "iXFgOBjk860",
-    "drowningStartSecs": 15.212946,
-    "whistleSecs": 39.515772,
-    "swimmerSavedSecs": 41.586436,
-    "findBoxStyle": "left: 297px; top: -381px; position: relative; width: 118px; height: 97px;"
-  }];
-
   var gameState = pickRandom(videos);
   var player;
 
